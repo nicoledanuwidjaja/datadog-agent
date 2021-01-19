67ae710bb72062f0d55ee1a2a594a221b50e9288
Lénaïc Huard <lenaic.huard@datadoghq.com>
2021-01-19 16:16:15 +0100

    Upgrade Argo

--- test/e2e/scripts/run-instance/23-argo-get.sh
+++ test/e2e/scripts/run-instance/23-argo-get.sh
@@ -18,36 +18,17 @@ fi
 
 set +x
 
-# `argo get` command outputs some utf-8 characters that are breaking GitLab
-# The below hack can be removed once the following PR is merged and released:
-# https://github.com/argoproj/argo/pull/4449
-function filter_argo_output() {
-    if locale -k LC_CTYPE | grep -qi 'charmap="utf-\+8"'; then
-        cat
-    else
-        oldstate=$(set +o)
-        set +x
-        sed 's/◷/Pending  /g;
-             s/●/Running  /g;
-             s/✔/Succeeded/g;
-             s/○/Skipped  /g;
-             s/✖/Failed   /g;
-             s/⚠/Error    /g;
-             s/ǁ/Suspend  /g;
-             s/·/+/g;
-             s/└/`/g;
-             s/├/|/g;'
-        eval "$oldstate"
-    fi
-}
+if locale -k LC_CTYPE | grep -qi 'charmap="utf-\+8"'; then
+    no_utf8_opt='--no-utf8'
+fi
 
 for workflow in $(./argo list -l workflows.argoproj.io/phase=Succeeded -o name); do
-    ./argo get "$workflow" | filter_argo_output
+    ./argo get "${no_utf8_opt:-}" "$workflow"
 done
 
 EXIT_CODE=0
 for workflow in $(./argo list -l workflows.argoproj.io/phase=Failed -o name); do
-    ./argo get "$workflow" | filter_argo_output
+    ./argo get "${no_utf8_opt:-}" "$workflow"
     EXIT_CODE=2
 done
 
